cb1a468b4ec25323d657085a3b9487ac40fe0231
ReRe-Create <liureanna@gmail.com>
2026-01-15 21:39:36 -0500

    styled time and removed AM/PM

--- src/components/time.jsx
+++ src/components/time.jsx
@@ -1,19 +1,29 @@
+import { useState, useEffect } from "react";
+import "/src/CSS/index.css";
+
 function Timecomponent() {
 
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const timestring = time.toLocaleTimeString([],{
+         hour: 'numeric',
+         minute: '2-digit'
+    }).replace(/AM|PM/, '').trim();
+    
     return (
-        <div id="time">
-            <h1>{updateTime}</h1>
+        <div className="time">
+            <h1>{timestring}</h1>
         </div>
     );
-}  
 
-function updateTime() {
-    const now = new Date();
-    let timestring = now.toLocaleTimeString([],
-        { hour: 'numeric', minute: '2-digit' });
-    document.getElementById("time").innerHTML = `<h1>${timestring}</h1>`;
 }
-setInterval(updateTime, 60000);
 
 export default Timecomponent;
-
